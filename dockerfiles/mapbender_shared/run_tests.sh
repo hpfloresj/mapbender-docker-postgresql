926441d7c3929d46d162e830c228e710c858680d
cioddi <maxtobiasweber@gmail.com>
2021-03-17 16:59:14 +0100

    Remove run_tests.sh

--- dockerfiles/mapbender_shared/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /srv/www/mapbender/
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer --version=1.0.0
-composer require --dev symfony/phpunit-bridge
-php -d memory_limit=4G $(which composer) update symfony/phpunit-bridge
